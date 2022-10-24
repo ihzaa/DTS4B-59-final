d2912313739842badf8d4883093ef9372163f8ca
muhamadrizkymaulana11 <muhamadrizkymaulana11@gmail.com>
2022-10-24 09:49:48 +0700

    fixed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
+import HtmlTooltip from "./HtmlTooltip";
+import { Chip, Divider, Rating, Grid } from "@mui/material";
+import PreviewIcon from "@mui/icons-material/Preview";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 const styles = {
   card: {
